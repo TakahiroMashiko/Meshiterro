7bdd80d216f3de90c544496928def93d7f504023
TakahiroMashiko <melto393122@gmail.com>
2021-04-10 13:38:36 +0000

    [Add] 4-4 4章【コメント・いいね機能を作ろう】-ページング機能を作成する-ページャを実装する

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -13,7 +13,8 @@ class PostImagesController < ApplicationController
   end
 
   def index
-    @post_images = PostImage.all
+    # 新しい順に取得する
+    @post_images = PostImage.page(params[:page]).reverse_order
   end
 
   def show
